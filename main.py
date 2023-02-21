0712ba153ca81f4f8aef1639430b818124c99c74
Lazkjaz <87209586+Laz9873@users.noreply.github.com>
2023-02-21 15:41:19 -0500

    Feat: Button support Homescreen

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
-KV = '''
-MDScreen:
+from kivy.lang import Builder
+import kivymd.uix.boxlayout
+from kivymd.app import MDApp
+from kivy.core.window import Window
 
-    MDLabel:
-        text: "Hello, World!"
-        halign: "center"
+
+Window.size = (360, 600)
+
+
+KV = '''
+
+MDFloatLayout:
+    MDSwitch:
+        pos_hint: {'center_x': .40, 'center_y': .15}
+
+    MDRaisedButton:
+        text: "Today's Workout"
+        md_bg_color: "red"
+        pos_hint: {'center_x': .5,  'center_y': .35}
+        
+
+
 '''
 
-from kivy.lang import Builder
 
-from kivymd.app import MDApp
-
-
-class MainApp(MDApp):
+class Healthware(MDApp):
     def build(self):
+        
+        self.theme_cls.primary_palette = "Green"
+        self.theme_cls.primary_hue = "900"
+        self.theme_cls.theme_style = "Dark"
+        
         return Builder.load_string(KV)
 
-    def on_start(self):
-        self.fps_monitor_start()
 
-
-MainApp().run()
+Healthware().run()
