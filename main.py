33a0f1248795b110e39a11ce14eea885b4661083
Lazkjaz <87209586+Laz9873@users.noreply.github.com>
2023-03-28 03:26:56 -0400

    feat: responsive screen size

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ import kivymd.uix.boxlayout
 from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
 from kivymd.uix.card import MDCard 
-from kivy.core.window import Window
 import kivymd.uix.button.button
 from kivy.uix.screenmanager import Screen, ScreenManager
+from kivymd.theming import ThemeManager
 
 
-Window.size = (360, 600)
-
-Builder.load_file("healthware.kv")
+Builder.load_file("hware.kv")
 
 class HomeScreen(Screen):
     pass
@@ -26,11 +24,21 @@ class Healthware(MDApp):
     def __init__(self, **kwargs):
         super(Healthware, self).__init__(**kwargs)
         self.previous_screen = ""
+        self.theme_cls = ThemeManager()
+        self.theme_cls.primary_palette = "BlueGray"
+        self.theme_cls.accent_palette = "Gray"
+        self.theme_cls.theme_style = "Dark"
 
     def build(self):
         
         return RootWidget()
 
-
-
+ 
 Healthware().run()
+
+
+
+
+# to do
+# fix the size of the card view
+# implement the themeing options
